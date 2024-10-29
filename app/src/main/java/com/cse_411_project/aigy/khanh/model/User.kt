6ef26cc35357b2f001fc5d716b86e6f1c7e42b53
Khánh Đinh <dgkhanh2003@gmail.com>
2024-10-30 01:22:21 +0700

    feat: User model

--- app/src/main/java/com/cse_411_project/aigy/khanh/model/User.kt
+++ app/src/main/java/com/cse_411_project/aigy/khanh/model/User.kt
@@ -2,10 +2,12 @@ package com.cse_411_project.aigy.khanh.model
 
 class User {
     val decentralization: String? = null
-    val email: String? = null
-    val name: String? = null
+    var email: String? = null
+    var fullName: String? = null
     val online: Boolean? = null
-    val phone: String? = null
-    val uid: String? = null
-    val urlImg: String? = null
+    var phoneNumber: String? = null
+    var uid: String? = null
+    var urlImage: String? = null
+    var referralCount: Int? = null
+    var conversationList: List<String>? = null
 }
