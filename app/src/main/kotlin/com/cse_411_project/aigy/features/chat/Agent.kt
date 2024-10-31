a3ffb1d68bbc39e6f949a04750accc24c07349bd
DNA <ducnguyen6693@gmail.com>
2024-11-01 00:32:15 +0700

    fix:Agent

--- app/src/main/kotlin/com/cse_411_project/aigy/features/chat/Agent.kt
+++ app/src/main/kotlin/com/cse_411_project/aigy/features/chat/Agent.kt
@@ -1,27 +1,8 @@
-package com.cse_411_project.aigy.features.chat;
+package com.cse_411_project.aigy.features.chat
 
-public class Agent {
-    private String title;
-    private String description;
-    private String category;
-    private String author;
-
-    public Agent(String title, String description, String category, String author) {
-        this.title = title;
-        this.description = description;
-        this.category = category;
-        this.author = author;
-    }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-
-    public String getCategory() { return category; }
-    public void setCategory(String category) { this.category = category; }
-
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
-}
+data class Agent(
+        var title: String,
+        var description: String,
+        var category: String,
+        var author: String
+)
