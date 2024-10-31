5b33fe3b830ecf15828d1f68eb4c0143fe71b420
GitTuanKiet <tuank1792@gmail.com>
2024-11-01 00:27:06 +0700

    remove process bar in baseActivity

--- app/src/main/kotlin/com/cse_411_project/aigy/core/platform/BaseFragment.kt
+++ app/src/main/kotlin/com/cse_411_project/aigy/core/platform/BaseFragment.kt
@@ -36,12 +36,6 @@ abstract class BaseFragment : Fragment() {
 
     internal fun firstTimeCreated(savedInstanceState: Bundle?) = savedInstanceState == null
 
-    internal fun showProgress() = progressStatus(View.VISIBLE)
-
-    internal fun hideProgress() = progressStatus(View.GONE)
-
-    private fun progressStatus(viewStatus: Int) =
-        with(activity) { if (this is BaseActivity) this.getProcessBar().visibility = viewStatus }
 
     internal fun notify(@StringRes message: Int) =
         Snackbar.make(viewContainer, message, Snackbar.LENGTH_SHORT).show()
